3145a4abf9b7b4c0c76536c8d213a7d8e35d2518
Bence Kiss <bbence.kiss@gmail.com>
2019-11-08 16:37:41 +0100

    :card_file_box: create db migration for issues

---
+++ db/migrate/20191108152532_create_issues.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+# Create Issues table
+class CreateIssues < ActiveRecord::Migration[6.0]
+  def change
+    create_table :issues do |t|
+      t.string  :name
+      t.text    :description
+      t.integer :turnaround
+
+      t.timestamps
+    end
+  end
+end
